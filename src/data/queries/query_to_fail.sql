c0a2b90abb15f3802d9d92003dbfd9df434daf85
ff-l <33835310+ff-l@users.noreply.github.com>
2018-10-10 13:47:57 +0100

    Test queries, one with errors

--- src/data/queries/query_to_fail.sql
+++ src/data/queries/query_to_fail.sql
@@ -9,7 +9,7 @@ FROM (
   SELECT
     *
   FROM (
-    SELECT
+    ----SELECT
       CONCAT(fullVisitorId,"-",STRING(visitId),"-",STRING(visitNumber),"-",STRING(TIMESTAMP(INTEGER(visitStartTime*1000000)))) AS sessionId,
       GROUP_CONCAT(CONCAT(pagePath,"::",CONCAT(IFNULL(hits.eventInfo.eventCategory,"NULL"),"//",IFNULL(hits.eventInfo.eventAction,"NULL"))),">>") OVER (PARTITION BY fullVisitorId, visitId ORDER BY hits.hitNumber rows BETWEEN unbounded preceding AND unbounded following ) AS Sequence,
       TrafficSource,
@@ -30,17 +30,7 @@ FROM (
         hits.eventInfo.eventAction,
         date AS Date,
         hits.type,
-        hits.eventInfo.eventCategory,
-      FROM
-        TABLE_DATE_RANGE([govuk-bigquery-analytics:87773428.ga_sessions_],
-          TIME_STAMP))
-    WHERE
-      PageSeq_Length > 1
-  GROUP BY
-    sessionId,
-    Sequence,
-    TrafficSource,
-    TrafficMedium,
+        hits
     Date,
     Actions_Length,
     PageSeq_Length)
