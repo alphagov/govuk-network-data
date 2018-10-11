19c08efe55d26bf1648ca01b68372bd5b8f6bcb8
ff-l <33835310+ff-l@users.noreply.github.com>
2018-10-11 14:22:34 +0100

    Preprocessing steps for dataset initialization
    Implemented: BQ user journey string listification
    (that is not a word), event counting (categories and actions),
    loop-related functions (collapsing, detection, occurrence counts)
    TODO: add iteration over multiple dataframes (daily data)

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,148 @@
 # # -*- coding: utf-8 -*-
-# import click
-# import logging
-# from pathlib import Path
-# from dotenv import find_dotenv, load_dotenv
-#
-# 
-# @click.command()
-# @click.argument('input_filepath', type=click.Path(exists=True))
-# @click.argument('output_filepath', type=click.Path())
-# def main(input_filepath, output_filepath):
-#     """ Runs data processing scripts to turn raw data from (../raw) into
-#         cleaned data ready to be analyzed (saved in ../processed).
-#     """
-#     logger = logging.getLogger(__name__)
-#     logger.info('making final data set from raw data')
-#
-#
-# if __name__ == '__main__':
-#     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-#     logging.basicConfig(level=logging.INFO, format=log_fmt)
-#
-#     # not used in this stub but often useful for finding various files
-#     project_dir = Path(__file__).resolve().parents[2]
-#
-#     # find .env automagically by walking up directories until it's found, then
-#     # load up the .env entries as environment variables
-#     load_dotenv(find_dotenv())
-#
-#     main()
+
+import argparse
+import logging.config
+import os
+import pprint
+import re
+from collections import Counter
+
+
+# Transform raw SQL BigQuery string to list of page/event tuples
+def clean_tuple(x):
+    return [re.sub(r":|\"|\'", "", xs) for xs in x]
+
+
+def bq_journey_to_list(x):
+    journey_list = []
+    for xs in x.split(">>"):
+        page_event_tup = clean_tuple(xs.split("::"))
+        if len(page_event_tup) == 2:
+            journey_list.append(tuple(page_event_tup))
+        else:
+            journey_list.append(("::".join(page_event_tup[:-1]), page_event_tup[-1]))
+    return journey_list
+
+
+# Requires rework: this function deloops the sequence (A >> A >> A => A).
+# If you don't want this, uncomment the else.
+# def deloop_event_page_list(x):
+#     position_dict = []
+#     for i,(page,event) in enumerate(x):
+# #         print(i,(page,event))
+#         if i==0 or page != x[i-1][0]:
+#             index = i
+#             if len(position_dict)>0:
+#                 index = position_dict[-1][0]
+#             position_dict.append((index+1,(page,event)))
+#         elif  page == x[i-1][0] and event!='NULL//NULL':
+#             prev_event = position_dict[-1]
+#             position_dict.append((prev_event[0],(prev_event[1][0],event)))
+#         elif  page == x[i-1][0] and event =='NULL//NULL' and position_dict[-1][0] != 'NULL//NULL':
+#             position_dict.append((prev_event[0],(prev_event[1][0],event)))
+# #         else:
+# #             position_dict.append((index+1,(page,event)))
+#     return position_dict
+
+
+def split_event_cat_act(event_list):
+    return [tuple(event.split("//")) for event in event_list]
+
+
+def extract_pe_components(page_event_list, i):
+    return [page_event[i] for page_event in page_event_list]
+
+
+# Counts for events
+def count_event_categories(event_list):
+    return len(set([cat for cat, _ in event_list]))
+
+
+def count_event_actions(event_list, kind, category):
+    return [action for cat,action in event_list if cat == category].count(kind)
+
+
+def aggregate_event_categories(event_list):
+    return [(key, value) for key, value in Counter([cat for cat, _ in event_list]).items()]
+
+
+def aggregate_event_actions(event_list):
+    return [(key, value) for key, value in Counter([(cat, act) for cat, act in event_list]).items()]
+
+
+# Transform metadata lists to dictionary aggregates
+def list_to_dict(x):
+    local_dict = Counter()
+    for xs in x.split(','):
+        local_dict[xs]+=1
+    return list(local_dict.items())
+
+# TODO: needs more work, right now it is dependant on hardcoded df column specification
+def zip_aggregate_metadata(user_journey_df):
+    col = []
+    for tup in user_journey_df.itertuples():
+        locs = tup.Locations.split(',')
+        langs = tup.Languages.split(',')
+        devs = tup.DeviceCategories.split(',')
+        zipped_meta_counter = Counter()
+        for loc,lang,dev in zip(locs,langs,devs):
+            zipped_meta_counter[(loc,lang,dev)] += 1
+        col.append(list(zipped_meta_counter.items()))
+
+    user_journey_df['AggMeta'] = col
+
+
+# Loop-related functions
+def has_loop(page_list):
+    return any(i == j for i, j in zip(page_list, page_list[1:]))
+
+
+def collapse_loop(page_list):
+    return [node for i, node in enumerate(page_list) if i == 0 or node != page_list[i - 1]]
+
+
+def add_loop_columns(user_journey):
+    logger.info("Collapsing loops...")
+    user_journey['Sequence_List_No_Loops'] = user_journey['Sequence_List'].map(collapse_loop)
+    # logger.info("Has loop...")
+    # user_journey['Has_Loop'] = user_journey['Sequence_List'].map(has_loop)
+    # logger.info("To string...")
+    # user_journey['Sequence_No_Loops'] = user_journey['Sequence_List_No_Loops'].map(list_to_path_string)
+    logger.info("Aggregating de-looped journey occurrences...")
+    user_journey['Occurrences_No_Loop'] = user_journey.groupby('Sequence_No_Loop') \
+        ['Occurrences'].transform('sum')
+
+
+# repetitions
+def has_repetition(journey_list):
+    return len(set(journey_list)) != len(journey_list)
+
+
+if __name__ == "__main__":
+    print(split_event_cat_act(["ffyesno//yes","NULL//NULL","NULL//NULL"]))
+
+    eventlist = [("NULL","NULL"),("ffyesno","yes"),("ffyesno","no"),("ffman","no"),("ffyesno","no")]
+
+    print(aggregate_event_actions(eventlist))
+    print(aggregate_event_categories(eventlist))
+    
+    # parser = argparse.ArgumentParser(description='BigQuery extractor module')
+    # parser.add_argument('source_dir', help='Source directory for input dataframe file(s).')
+    # parser.add_argument('dest_dir', help='Specialized destination directory for output dataframe file.')
+    # parser.add_argument('filename', help='Naming convention for resulting merged dataframe file.')
+    # args = parser.parse_args()
+    #
+    # Logger setup
+    LOGGING_CONFIG = os.getenv("LOGGING_CONFIG")
+    logging.config.fileConfig(LOGGING_CONFIG)
+    logger = logging.getLogger('make_dataset')
+    #
+    # DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), 'data')
+    # source_dir = ""
+    # dest_dir = ""
+    #
+    # logger.info("Loading data")
+    #
+    # to_load = [os.path.join(source_dir, file) for file in os.listdir(source_dir)]
+    #
+    # pprint.pprint(to_load)
