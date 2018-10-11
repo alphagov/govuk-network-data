4602fb0f09f54336bbe4a22f736bc44ebf17f58e
ff-l <33835310+ff-l@users.noreply.github.com>
2018-10-11 15:52:45 +0100

    Generate page_list and event_list vals, de-looping
    and reindexing function should now work fine. Added
    a silly test(). Commented stuff out in main so it
    doesn't break during test runs

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,11 +1,10 @@
 # # -*- coding: utf-8 -*-
 
-import argparse
 import logging.config
 import os
-import pprint
 import re
 from collections import Counter
+import numpy as np
 
 
 # Transform raw SQL BigQuery string to list of page/event tuples
@@ -13,10 +12,15 @@ def clean_tuple(x):
     return [re.sub(r":|\"|\'", "", xs) for xs in x]
 
 
-def bq_journey_to_list(x):
+def bq_journey_to_pe_list(bq_journey_string):
+    """
+
+    :param bq_journey_string:
+    :return:
+    """
     journey_list = []
-    for xs in x.split(">>"):
-        page_event_tup = clean_tuple(xs.split("::"))
+    for hit in bq_journey_string.split(">>"):
+        page_event_tup = clean_tuple(hit.split("::"))
         if len(page_event_tup) == 2:
             journey_list.append(tuple(page_event_tup))
         else:
@@ -24,69 +28,114 @@ def bq_journey_to_list(x):
     return journey_list
 
 
-# Requires rework: this function deloops the sequence (A >> A >> A => A).
-# If you don't want this, uncomment the else.
-# def deloop_event_page_list(x):
-#     position_dict = []
-#     for i,(page,event) in enumerate(x):
-# #         print(i,(page,event))
-#         if i==0 or page != x[i-1][0]:
-#             index = i
-#             if len(position_dict)>0:
-#                 index = position_dict[-1][0]
-#             position_dict.append((index+1,(page,event)))
-#         elif  page == x[i-1][0] and event!='NULL//NULL':
-#             prev_event = position_dict[-1]
-#             position_dict.append((prev_event[0],(prev_event[1][0],event)))
-#         elif  page == x[i-1][0] and event =='NULL//NULL' and position_dict[-1][0] != 'NULL//NULL':
-#             position_dict.append((prev_event[0],(prev_event[1][0],event)))
-# #         else:
-# #             position_dict.append((index+1,(page,event)))
-#     return position_dict
+def reindex_pe_list(page_event_list):
+    """
+    Reindex and de-loop page_event_list if necessary. Used when absolute hit position within journey
+    needs to be evaluated.
+    If that's the case, page_list and event_list generators should be run based on this list, not
+    page_event_list itself.
+    :param page_event_list:
+    :return:
+    """
+    if len(page_event_list) > 0:
+        position_dict = [(0, page_event_list[0])]
+        for i, (page, event) in enumerate(page_event_list[1:]):
+            # print(i)
+            if page != page_event_list[i][0]:
+                index = position_dict[-1][0]
+                position_dict.append((index + 1, (page, event)))
+            elif page == page_event_list[i][0] and (event != position_dict[-1][1][1]):
+                position_dict.append((position_dict[-1][0], (page, event)))
+        return position_dict
+    return np.NaN
 
 
 def split_event_cat_act(event_list):
+    """
+
+    :param event_list:
+    :return:
+    """
     return [tuple(event.split("//")) for event in event_list]
 
 
 def extract_pe_components(page_event_list, i):
+    """
+
+    :param page_event_list:
+    :param i:
+    :return:
+    """
     return [page_event[i] for page_event in page_event_list]
 
 
 # Counts for events
 def count_event_categories(event_list):
+    """
+
+    :param event_list:
+    :return:
+    """
     return len(set([cat for cat, _ in event_list]))
 
 
-def count_event_actions(event_list, kind, category):
-    return [action for cat,action in event_list if cat == category].count(kind)
+def count_event_actions(event_list, category, action):
+    """
+
+    :param event_list:
+    :param action:
+    :param category:
+    :return:
+    """
+    return [action for cat, action in event_list if cat == category].count(action)
 
 
 def aggregate_event_categories(event_list):
+    """
+
+    :param event_list:
+    :return:
+    """
     return [(key, value) for key, value in Counter([cat for cat, _ in event_list]).items()]
 
 
 def aggregate_event_actions(event_list):
-    return [(key, value) for key, value in Counter([(cat, act) for cat, act in event_list]).items()]
+    """
+
+    :param event_list:
+    :return:
+    """
+    return list(Counter([(cat, act) for cat, act in event_list]).items())
 
 
 # Transform metadata lists to dictionary aggregates
-def list_to_dict(x):
-    local_dict = Counter()
-    for xs in x.split(','):
-        local_dict[xs]+=1
-    return list(local_dict.items())
+def list_to_dict(metadata_list):
+    # local_dict = Counter()
+    # for xs in x:
+    #     local_dict[xs] += 1
+    # return list(local_dict.items())
+    return list(Counter([xs for xs in metadata_list]).items())
+
+
+def str_to_dict(metadata_str):
+    return list_to_dict(metadata_str.split(','))
+
 
 # TODO: needs more work, right now it is dependant on hardcoded df column specification
 def zip_aggregate_metadata(user_journey_df):
+    """
+
+    :param user_journey_df:
+    :return:
+    """
     col = []
     for tup in user_journey_df.itertuples():
         locs = tup.Locations.split(',')
         langs = tup.Languages.split(',')
         devs = tup.DeviceCategories.split(',')
         zipped_meta_counter = Counter()
-        for loc,lang,dev in zip(locs,langs,devs):
-            zipped_meta_counter[(loc,lang,dev)] += 1
+        for loc, lang, dev in zip(locs, langs, devs):
+            zipped_meta_counter[(loc, lang, dev)] += 1
         col.append(list(zipped_meta_counter.items()))
 
     user_journey_df['AggMeta'] = col
@@ -94,14 +143,29 @@ def zip_aggregate_metadata(user_journey_df):
 
 # Loop-related functions
 def has_loop(page_list):
+    """
+
+    :param page_list:
+    :return:
+    """
     return any(i == j for i, j in zip(page_list, page_list[1:]))
 
 
 def collapse_loop(page_list):
+    """
+
+    :param page_list:
+    :return:
+    """
     return [node for i, node in enumerate(page_list) if i == 0 or node != page_list[i - 1]]
 
 
 def add_loop_columns(user_journey):
+    """
+
+    :param user_journey:
+    :return:
+    """
     logger.info("Collapsing loops...")
     user_journey['Sequence_List_No_Loops'] = user_journey['Sequence_List'].map(collapse_loop)
     # logger.info("Has loop...")
@@ -114,18 +178,72 @@ def add_loop_columns(user_journey):
 
 
 # repetitions
-def has_repetition(journey_list):
-    return len(set(journey_list)) != len(journey_list)
+def has_repetition(page_list):
+    """
+    Check if a list of page hits contains a page repetition (A >> B >> A) == True
+    Run on journeys with collapsed loops so stuff like A >> A >> B are not captured as a repetition
+    :param page_list: list of page hits derived from BQ user journey
+    :return: True if there is a repetition
+    """
+    return len(set(page_list)) != len(page_list)
 
 
-if __name__ == "__main__":
-    print(split_event_cat_act(["ffyesno//yes","NULL//NULL","NULL//NULL"]))
+# Network things, should probably be moved somewhere else
+def start_end_page(page_list):
+    """
 
-    eventlist = [("NULL","NULL"),("ffyesno","yes"),("ffyesno","no"),("ffman","no"),("ffyesno","no")]
+    :param page_list:
+    :return:
+    """
+    return page_list[0], page_list[-1]
+
+
+def subpaths_from_list(page_list):
+    """
+
+    :param page_list:
+    :return:
+    """
+    return [[page, page_list[i + 1]] for i, page in enumerate(page_list) if i < len(page_list) - 1]
+
+
+def start_page(page_list):
+    return page_list[0]
+
+
+def start_end_subpath_list(subpath_list):
+    return subpath_list[0][0], subpath_list[-1][-1]
+
+
+def test():
+    print(split_event_cat_act(["ffyesno//yes", "NULL//NULL", "NULL//NULL"]))
+
+    print("\n=======\n")
+    eventlist = [("NULL", "NULL"), ("ffyesno", "yes"), ("ffyesno", "no"), ("ffman", "no"), ("ffyesno", "no")]
 
     print(aggregate_event_actions(eventlist))
     print(aggregate_event_categories(eventlist))
-    
+
+    # eventlist2 = [("NULL", "NULL")]
+    eventlist2 = []
+    print(aggregate_event_actions(eventlist2))
+    print(aggregate_event_categories(eventlist2))
+
+    pelist = [("p1", "ffyesno//yes"), ("p1", "NULL//NULL"), ("p1", "NULL//NULL")]
+    pelist2 = [("p1", "ffyesno//yes"), ("p1", "NULL//NULL"), ("p1", "NULL//NULL"), ("p1", "NULL//jjj")]
+
+    print("\n=======\n")
+    print(reindex_pe_list(pelist))
+    print(reindex_pe_list(pelist2))
+
+    print("\n=======\n")
+    print(split_event_cat_act(extract_pe_components(pelist, 1)))
+
+    print(str_to_dict("1,2,3,4,3,4,2,23,2,32,3,23"))
+
+
+if __name__ == "__main__":
+    test()
     # parser = argparse.ArgumentParser(description='BigQuery extractor module')
     # parser.add_argument('source_dir', help='Source directory for input dataframe file(s).')
     # parser.add_argument('dest_dir', help='Specialized destination directory for output dataframe file.')
