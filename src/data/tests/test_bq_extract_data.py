708d5d9965a2db477185e191720aa2dcd0ebfd61
Matthew Gregory <matthew.gregory@digital.cabinet-office.gov.uk>
2018-10-15 16:30:09 +0100

    Add read_query test reading toy query

--- src/data/tests/test_bq_extract_data.py
+++ src/data/tests/test_bq_extract_data.py
@@ -4,3 +4,9 @@ import bq_extract_data
 
 def test_find_query():
     assert bq_extract_data.find_query("test_bq_extract_data.py", "./tests") == "./tests/test_bq_extract_data.py"
+
+
+# test removing linebreaks from sql query file
+# add space for line breaks
+def test_read_query():
+    assert bq_extract_data.read_query("./tests/test.sql") == "SELECT * FROM tables WHERE thing < 5"
