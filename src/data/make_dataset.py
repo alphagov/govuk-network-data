fb3d6f8a4e48b67319ea9fcff091b402344e7ab0
ff-l <33835310+ff-l@users.noreply.github.com>
2018-10-22 17:17:12 +0100

    Drop page_sequence occurring once, instead of page+event
    hit journeys. Needs testing.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -18,7 +18,9 @@ AGGREGATE_COLUMNS = ['Languages', 'Locations', 'DeviceCategories',
 COUNTABLE_AGGREGATE_COLUMNS = ['Languages', 'Locations', 'DeviceCategories', 'TrafficSources',
                                'TrafficMediums', 'NetworkLocations']
 SLICEABLE_COLUMNS = ['Occurrences', 'Languages', 'Locations', 'DeviceCategories', 'TrafficSources',
-                     'TrafficMediums', 'NetworkLocations']
+                     'TrafficMediums', 'NetworkLocations', 'Dates']
+DROPABLE_COLS = ['Page_Event_List', 'Page_List']
+
 FEWER_THAN_CPU = False
 DROP_INFREQ = False
 MAX_DEPTH = -1
@@ -61,8 +63,9 @@ def sequence_preprocess(user_journey_df):
     logger.info("BQ Sequence string to Page_Event_List...")
     user_journey_df['Page_Event_List'] = user_journey_df['Sequence'].map(bq_journey_to_pe_list)
     logger.info("Page_Event_List to Page_List...")
-    user_journey_df['Page_List'] = user_journey_df['Page_Event_List'].map(lambda x: extract_pe_components(x, 1))
+    user_journey_df['Page_List'] = user_journey_df['Page_Event_List'].map(lambda x: extract_pe_components(x, 0))
     # logger.info("Page_Event_List to Event_List...")
+    user_journey_df['PageSequence'] = user_journey_df['Page_List'].map(lambda x: ">>".join(x))
     # user_journey_df['Event_List'] = user_journey_df['Page_Event_List'].map(lambda x: extract_pe_components(x, 1))
 
 
@@ -121,21 +124,19 @@ def drop_duplicate_rows(df_slice, multiple_dfs):
 def sliced_mass_preprocess(code_df_slice, depth, multiple_dfs):
     code = code_df_slice[0]
     df_slice = code_df_slice[1]
-    # print(df_slice.shape)
-    # logger.info("Mass preprocessing {}...".format(df_slice.columns))
-    # aggregate-able columns
     if df_slice.columns[1] in COUNTABLE_AGGREGATE_COLUMNS:
         logger.info("Aggregating {}...".format(df_slice.columns[1]))
         sliced_groupby_meta(df_slice, depth, multiple_dfs)
     elif "Occurrences" in df_slice.columns[1]:
         logger.info("Occurrences...")
         df_slice['Occurrences'] = df_slice.groupby('Sequence')['Occurrences'].transform('sum')
+        df_slice['Page_Seq_Occurrences'] = df_slice.groupby('PageSequence')['Occurrences'].transform('sum')
         drop_duplicate_rows(df_slice, multiple_dfs)
         # print(DEPTH_LIM, MAX_DEPTH, DROP_INFREQ)
         if (depth >= DEPTH_LIM and MAX_DEPTH >= 2) and DROP_INFREQ:
             bef = df_slice.shape[0]
             # logger.info("Current # of rows: {}. Dropping journeys occurring only once..".format(bef))
-            df_slice = df_slice[df_slice.Occurrences > 1]
+            df_slice = df_slice[df_slice.Page_Seq_Occurrences > 1]
             after = df_slice.shape[0]
             logger.info("Dropped {} one-off rows.".format(bef - after))
     return code, df_slice
@@ -143,7 +144,10 @@ def sliced_mass_preprocess(code_df_slice, depth, multiple_dfs):
 
 def read_file(filename):
     logging.info("Reading: {}".format(filename))
-    return pd.read_csv(filename, compression="gzip")
+    df = pd.read_csv(filename, compression="gzip")
+    sequence_preprocess(df)
+    df.drop(DROPABLE_COLS, axis=1, inplace=True)
+    return df
 
 
 def partition_list(x, chunks):
@@ -176,8 +180,15 @@ def del_var(x):
 
 
 def slice_dataframe(df):
-    return [[df.columns.get_loc("Sequence"), df.columns.get_loc(col)] for col in SLICEABLE_COLUMNS if
-            col in df.columns]
+    sliced_df = []
+    for col in SLICEABLE_COLUMNS:
+        if col in df.columns:
+            if col == "Occurrences":
+                sliced_df.append(
+                    [df.columns.get_loc("Sequence"), df.columns.get_loc("PageSequence"), df.columns.get_loc(col)])
+            else:
+                sliced_df.append([df.columns.get_loc("Sequence"), df.columns.get_loc(col)])
+    return sliced_df
 
 
 def slice_many_df(df_list, ordered=False):
@@ -216,8 +227,6 @@ def conditional_pre_gpb_drop(df_occ_slice, df_meta_slice):
         for df_code_j, df_slice_j in df_meta_slice:
             if df_code_i == df_code_j:
                 seq_occ = df_slice_i.Sequence.values
-                # print("original", seq_occ.shape)
-                # print("bef", df_slice_j.shape)
                 df_slice_j.query("Sequence.isin(@seq_occ)", inplace=True)
                 # print("after", df_slice_j.shape)
     return df_meta_slice
@@ -268,7 +277,7 @@ def distribute_df_slices(pool, dflist, chunks, depth=0, additional=None):
 
         # print("THEM COLUMNS", [(new.columns, new.shape) for _, new in new_list])
         new_list = merge_sliced_df(new_list, len(partitions))
-         # print("THEM COLUMNS", [(new.columns, new.shape) for new in new_list])
+        # print("THEM COLUMNS", [(new.columns, new.shape) for new in new_list])
         return distribute_df_slices(pool, new_list, int(chunks / 2), depth + 1, additional)
     else:
         return dflist[0]
@@ -304,7 +313,7 @@ def multiprocess_make(files, destination, final_filename):
     FEWER_THAN_CPU = num_chunks == len(files) if not batching else batch_size + 1 <= NUM_CPU
     MAX_DEPTH = compute_max_depth(files, num_chunks, 0) if not batching else compute_max_depth([0] * (batch_size + 1),
                                                                                                num_chunks, 0)
-    logger.info("BATCH_SIZE: {} MAX_DEPTH: {} NUM_FILES: {}".format(batch_size, MAX_DEPTH,len(files)))
+    logger.info("BATCH_SIZE: {} MAX_DEPTH: {} NUM_FILES: {}".format(batch_size, MAX_DEPTH, len(files)))
     logger.info("Using {} workers...".format(NUM_CPU))
     pool = Pool(NUM_CPU)
 
@@ -349,33 +358,6 @@ def process(files, pool, num_chunks, df_prev=None):
     return df
 
 
-# def test():
-#     print(split_event_cat_act(["ffyesno//yes", "NULL//NULL", "NULL//NULL"]))
-#
-#     print("\n=======\n")
-#     eventlist = [("NULL", "NULL"), ("ffyesno", "yes"), ("ffyesno", "no"), ("ffman", "no"), ("ffyesno", "no")]
-#
-#     print(aggregate_event_cat_act(eventlist))
-#     print(aggregate_event_cat(eventlist))
-#
-#     # eventlist2 = [("NULL", "NULL")]
-#     eventlist2 = []
-#     print(aggregate_event_cat_act(eventlist2))
-#     print(aggregate_event_cat(eventlist2))
-#
-#     pelist = [("p1", "ffyesno//yes"), ("p1", "NULL//NULL"), ("p1", "NULL//NULL")]
-#     pelist2 = [("p1", "ffyesno//yes"), ("p1", "NULL//NULL"), ("p1", "NULL//NULL"), ("p1", "NULL//jjj")]
-#
-#     print("\n=======\n")
-#     print(reindex_pe_list(pelist))
-#     print(reindex_pe_list(pelist2))
-#
-#     print("\n=======\n")
-#     print(split_event_cat_act(extract_pe_components(pelist, 1)))
-#
-#     print(str_to_dict("1,2,3,4,3,4,2,23,2,32,3,23"))
-
-
 def compute_max_depth(test_list, chunks, depth):
     partitions = partition_list(test_list, chunks)
     if len(test_list) > 1:
@@ -411,23 +393,6 @@ if __name__ == "__main__":
     source_dir = os.path.join(DOCUMENTS, "test1")
     name_stub = "user_network_paths_meta_2018-04-"
     to_load = [os.path.join(source_dir, file) for file in os.listdir(source_dir) if name_stub in file]
-
-    # test()
-    # tryout(to_load)
-    # lst = list(range(60))
-    # chunk = int(len(lst) / 2)
-    # print(chunk)
-    # print(list(range(len([1, 2]))))
-    # print("max depth", compute_max_depth(lst, chunk, 0) - 1)
-    # # list stuff
-    # print(list(itertools.product(COUNTABLE_AGGREGATE_COLUMNS, [1, 2, 3, 4])))
-    # lst = [1, 2]
-    # chunk_size = int(len(lst) / 2)
-    # print(len(lst), chunk_size)
-    # print([list(x) for x in np.array_split(lst, 3)])
-    # print(np.array_split(range(len(lst)), 2))
-    # test = [1, 2, 3, 4, 6]
-    # print("Final:", partition_list(test, 2))
 
     testout_dir = os.path.join(source_dir, "output")
 
