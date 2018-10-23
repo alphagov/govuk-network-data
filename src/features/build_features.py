628aa80595198a3a849388a1d523077bab400324
ff-l <33835310+ff-l@users.noreply.github.com>
2018-10-23 12:27:18 +0100

    Add documentation to functions for loop/repetition detection
    and event-related counters

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,17 +1,15 @@
 from collections import Counter
 
 
-# Loop-related functions
 def has_loop(page_list):
     """
-
-    :param page_list:
-    :return:
+    Check if a list of page hits contains an adjacent page loop (A >> A >> B) == True
+    :param page_list: list of page hits derived from BQ user journey
+    :return: True if there is a loop
     """
     return any(i == j for i, j in zip(page_list, page_list[1:]))
 
 
-# repetitions
 def has_repetition(page_list):
     """
     Check if a list of page hits contains a page repetition (A >> B >> A) == True
@@ -22,40 +20,43 @@ def has_repetition(page_list):
     return len(set(page_list)) != len(page_list)
 
 
-# Counts for events
+# Counters for events
 def count_event_cat(event_list):
     """
-
-    :param event_list:
-    :return:
+    TODO: possibly remove
+    Count different event categories present in an event_list. Includes "NULL" events coming from page
+    hits for the sake of completeness. Does not include frequency.
+    :param event_list: list of event tuples (eventCategory,eventAction)
+    :return: number of different eventCategories present
     """
     return len(set([cat for cat, _ in event_list]))
 
 
 def count_event_act(event_list, category, action):
     """
-
-    :param event_list:
-    :param category:
-    :param action:
-    :return:
+    TODO: possibly remove
+    Count number of specific eventActions given a specific eventCategory
+    :param event_list: list of event tuples (eventCategory,eventAction)
+    :param category: target eventCategory
+    :param action: target eventAction
+    :return: count
     """
     return [action for cat, action in event_list if cat == category].count(action)
 
 
 def aggregate_event_cat(event_list):
     """
-
-    :param event_list:
-    :return:
+    Return a dictionary-like list of eventCategory frequency counts.
+    :param event_list: list of event tuples (eventCategory,eventAction)
+    :return: dict-like list of frequencies [(eventCat1, freq_1),(eventCat2, freq_2),...]
     """
     return list(Counter([cat for cat, _ in event_list]).items())
 
 
 def aggregate_event_cat_act(event_list):
     """
-
-    :param event_list:
-    :return:
+    Return a dictionary-like list of (eventCategory,eventAction) frequency counts.
+    :param event_list: list of event tuples (eventCategory,eventAction)
+    :return: dict-like list of frequencies [((eventCat1,eventAction1) freq_1),((eventCat1,eventAction2) freq_2),...]
     """
     return list(Counter([(cat, act) for cat, act in event_list]).items())
