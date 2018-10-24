585d6bbb1891b9638ccf90258119cc9e1d8b5692
Matthew Gregory <matthew.gregory@digital.cabinet-office.gov.uk>
2018-10-24 11:54:13 +0100

    Unit test extract_pe_components

--- src/data/tests/test_preprocess.py
+++ src/data/tests/test_preprocess.py
@@ -9,5 +9,13 @@ def test_bq_journey_to_pe_list():
 
 def test_split_event():
     assert preprocess.split_event("eventCategory<:<eventAction") == ('eventCategory', 'eventAction')
+    assert preprocess.split_event("yesNoFeedbackForm<:<ffYesClick") == ("yesNoFeedbackForm", "ffYesClick")
 
 
+def test_extract_pe_components():
+    # get event category and the type of action taken when parameter i = 1
+    assert preprocess.extract_pe_components([('page1', 'eventCategory1<:<eventAction1'), ('page2', 'eventCategory2<:<eventAction2'), ('page3', 'eventCategory2<:<eventAction1')], 1) == [('eventCategory1', 'eventAction1'),
+ ('eventCategory2', 'eventAction2'),
+ ('eventCategory2', 'eventAction1')]
+    # should this return an empty list? Potential bug?
+    assert preprocess.extract_pe_components([('page1', 'eventCategory1<:<eventAction1'), ('page2', 'eventCategory2<:<eventAction2'), ('page3', 'eventCategory2<:<eventAction1')], i = 0) == []
