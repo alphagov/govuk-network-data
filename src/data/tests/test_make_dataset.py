ae0ba16468bfeca507b2593c5afc7cc4a863a011
Matthew Gregory <matthew.gregory@digital.cabinet-office.gov.uk>
2018-10-26 15:12:26 +0100

    Unit test str_to_dict

--- src/data/tests/test_make_dataset.py
+++ src/data/tests/test_make_dataset.py
@@ -5,3 +5,5 @@ import make_dataset
 def test_list_to_dict():
     assert make_dataset.list_to_dict(['Desktop', 'Tablet', 'Mobile', 'Desktop', 'Mobile', 'Desktop']) == [('Desktop', 3), ('Tablet', 1), ('Mobile', 2)]
 
+def test_str_to_dict():
+    assert make_dataset.str_to_dict("Mobile,Desktop,Mobile") == [("Mobile", 2),("Desktop", 1)]
