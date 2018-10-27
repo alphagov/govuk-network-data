4f638751d557139a2bbcf4297ded0514584edf32
Matthew Gregory <matthew.gregory@digital.cabinet-office.gov.uk>
2018-10-27 22:20:26 +0100

    Pandas test df is read-in

--- src/data/tests/test_make_dataset.py
+++ src/data/tests/test_make_dataset.py
@@ -1,6 +1,7 @@
 # to get correct relative path, run the following command from ./src/data/
 # python3 -m pytest tests/
 import make_dataset
+import pandas as pd
 
 def test_list_to_dict():
     assert make_dataset.list_to_dict(['Desktop', 'Tablet', 'Mobile', 'Desktop', 'Mobile', 'Desktop']) == [('Desktop', 3), ('Tablet', 1), ('Mobile', 2)]
@@ -10,3 +11,13 @@ def test_str_to_dict():
 
 def test_aggregate_dict():
     assert make_dataset.aggregate_dict([[("Desktop", 3), ("Tablet", 1), ("Mobile", 2)] + [("Desktop", 3), ("Tablet", 1), ("Mobile", 2)]]) == [('Desktop', 6), ('Tablet', 2), ('Mobile', 4)]
+
+
+# DATA PIPELINE
+# read some test data in
+def test_data_exists():
+    user_journey_df = None
+    user_journey_df = pd.read_pickle("./tests/user_journey_df.pkl")
+    assert user_journey_df is not None
+    assert user_journey_df.shape == (100, 7)
+
