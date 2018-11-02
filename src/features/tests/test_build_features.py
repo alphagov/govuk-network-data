ed473d56b3d641942b1da46ee1663aabb75bb5b2
ff-l <33835310+ff-l@users.noreply.github.com>
2018-11-02 15:53:04 +0000

    Simple tests for reps.

--- src/features/tests/test_build_features.py
+++ src/features/tests/test_build_features.py
@@ -10,8 +10,10 @@ def test_has_loop():
 
 def test_has_repetition():
     assert build_features.has_repetition(["page1", "page2", "page3"]) is False
+    # Yields true due to self-loop, should be run on collapsed-loop page lists
     assert build_features.has_repetition(["page1", "page1", "page1"]) is True
-    assert build_features.has_repetition(["page1", "page2", "page2"]) is True
+    assert build_features.has_repetition(["page1", "page2", "page3", "page1"]) is True
+    assert build_features.has_repetition(["page2", "page3", "page2"]) is True
 
 
 def test_count_event_cat():
@@ -19,23 +21,25 @@ def test_count_event_cat():
                                            ('eventCategory2', 'eventAction2'),
                                            ('eventCategory2', 'eventAction1')]) == 2
 
+
 def test_count_event_act():
     assert build_features.count_event_act([('eventCategory1', 'eventAction1'),
                                            ('eventCategory2', 'eventAction2'),
                                            ('eventCategory2', 'eventAction1')],
                                           category='eventCategory1', action='eventAction1') == 1
 
+
 def test_aggregate_event_count():
     assert build_features.aggregate_event_cat([('eventCategory1', 'eventAction1'),
                                                ('eventCategory2', 'eventAction2'),
-                                               ('eventCategory2', 'eventAction1')]) ==\
+                                               ('eventCategory2', 'eventAction1')]) == \
            [('eventCategory1', 1), ('eventCategory2', 2)]
 
 
 def test_aggregate_event_cat_act():
     assert build_features.aggregate_event_cat_act([('eventCategory1', 'eventAction1'),
                                                    ('eventCategory2', 'eventAction2'),
-                                                   ('eventCategory2', 'eventAction1')]) ==\
+                                                   ('eventCategory2', 'eventAction1')]) == \
            [(('eventCategory1', 'eventAction1'), 1),
             (('eventCategory2', 'eventAction2'), 1),
             (('eventCategory2', 'eventAction1'), 1)]
