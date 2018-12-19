f5bf0d37b7db22c1fbb2ebdd79df10eeae83fb15
ff-l <33835310+ff-l@users.noreply.github.com>
2018-12-19 12:35:49 +0000

    Function to recursively compute list of parent taxons

---
+++ src/data/taxon_translate.py
@@ -0,0 +1,23 @@
+def recursive_parenting(df, content_id, parent_content_id, parent_list):
+    """
+    
+    :param df:
+    :param content_id:
+    :param parent_content_id:
+    :param parent_list:
+    :return:
+    """
+    if isinstance(parent_content_id, float) and len(parent_list) == 0:
+        return []
+    elif isinstance(parent_content_id, float):
+        return [[thing, i + 1] for i, thing in enumerate(reversed(parent_list))]
+    else:
+        content_id = parent_content_id
+        parent_content_id = df[df.content_id == parent_content_id].iloc[0].parent_content_id
+        title = df[df.content_id == content_id].iloc[0].title
+        parent_list.append([content_id, parent_content_id, title])
+        return recursive_parenting(df, content_id, parent_content_id, parent_list)
+
+
+if __name__ == "__main__":
+    print()
