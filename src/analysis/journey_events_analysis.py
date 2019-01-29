e1107d7ad418a966cdf7788c4e2de472db75ab7b
ff-l <33835310+ff-l@users.noreply.github.com>
2019-01-29 11:19:26 +0000

    Standardize multiple stats computes

--- src/analysis/journey_events_analysis.py
+++ src/analysis/journey_events_analysis.py
@@ -2,8 +2,101 @@ import argparse
 import logging.config
 import os
 from ast import literal_eval
+from collections import Counter
+
+import pandas as pd
 from scipy import stats
 
+
+def device_count(x, device):
+    return sum([value for item, value in x if item == device])
+
+
+def has_related_event(x):
+    return all(cond in x for cond in ["relatedLinkClicked", "Related content"])
+
+
+def has_nav_event():
+    return False
+
+
+def map_counter(df):
+    df["DesktopCount"] = df['DeviceCategories'].map(lambda x: device_count(x, "desktop"))
+    df["MobileCount"] = df['DeviceCategories'].map(lambda x: device_count(x, "mobile"))
+
+
+def split_dataframe(df):
+    df["Has_Related"] = df["Sequence"].map(has_related_event)
+    df_related = df[df["Has_Related"]]
+    return df_related
+
+
+def weight_seq_length(page_lengths, occurrences, name):
+    length_occ = Counter()
+    for length, occ in zip(page_lengths, occurrences):
+        length_occ[length] += occ
+    data = []
+    for key, value in length_occ.items():
+        for i in range(value):
+            data.append(key)
+    return pd.Series(data, name=name)
+
+
+def chi2_test(vol_desk, vol_mobile, vol_mobile_rel, vol_desk_rel):
+    vol_mobile_no_rel = vol_mobile - vol_mobile_rel
+    vol_desk_no_rel = vol_desk - vol_desk_rel
+    obs = [[vol_mobile_rel, vol_mobile_no_rel], [vol_desk_rel, vol_desk_no_rel]]
+    return stats.chi2_contingency(obs)
+
+
+def compute_volumes(df, columns):
+    return (df[column].sum() for column in columns)
+
+
+def compute_percent(nums, denom):
+    return (round((num * 100) / denom, 2) for num in nums)
+
+
+def compute_stats(df, df_related, df_stats):
+    columns = ["Occurrences", "DesktopCount", "MobileCount"]
+    vol_all, vol_desk, vol_mobile = compute_volumes(df, columns)
+    vol_all_related, vol_desk_rel, vol_mobile_rel = compute_volumes(df_related, columns)
+
+    percent_from_desk, percent_from_mobile = compute_percent([vol_desk, vol_mobile], vol_all)
+
+    percent_related = round((vol_all_related * 100) / vol_all, 2)
+    percent_from_desk_rel = round((vol_desk_rel * 100) / vol_desk, 2)
+    percent_from_mobile_rel = round((vol_mobile_rel * 100) / vol_mobile, 2)
+    # shape_all = df.shape[0]
+    # shape_all_rel = df[df.Has_Related].shape[0]
+    # shape_desk = desktop_journeys.shape[0]
+    # shape_desk_rel = desktop_journeys[desktop_journeys.Has_Related].shape[0]
+    # shape_mobile = mobile_journeys.shape[0]
+    # shape_mobile_rel = mobile_journeys[mobile_journeys.Has_Related].shape[0]
+    #
+    # shapes = [shape_all, shape_all_rel,
+    #           shape_desk, shape_desk_rel,
+    #           shape_mobile, shape_mobile_rel]
+
+    df_stats["Volume"] = [vol_all, vol_all_related,
+                          vol_desk, vol_desk_rel,
+                          vol_mobile, vol_mobile_rel]
+    df_stats["Percentage"] = [100, percent_related,
+                              percent_from_desk, percent_from_desk_rel,
+                              percent_from_mobile, percent_from_mobile_rel]
+    # df_stats["Shape"] = shapes
+
+    a, b, c, d = chi2_test(vol_desk, vol_mobile, vol_mobile_rel, vol_desk_rel)
+
+    return df_stats
+
+
+def describe_dfs(to_eval):
+    descriptive = pd.DataFrame()
+    for length, occ, name in to_eval:
+        sr = weight_seq_length(length, occ, name).describe().apply(lambda x: format(x, '.3f'))
+        descriptive[sr.name] = sr
+    return descriptive
 
 
 def column_eval(cols, df):
@@ -16,77 +109,11 @@ def column_eval(cols, df):
         df['PageSeq_Length'] = df['Page_List'].map(len)
 
 
-def device_count(x, device):
-    return sum([value for item, value in x if item == device])
+def run(filename):
+    df = pd.read_csv(filename, sep="\t", compression="gzip")
 
-
-def is_related(x):
-    return all(cond in x for cond in ["relatedLinkClicked", "Related content"])
-
-
-def count_aggregate(df):
-    df["DesktopCount"] = df['DeviceCategories'].map(lambda x: device_count(x, "desktop"))
-    df["MobileCount"] = df['DeviceCategories'].map(lambda x: device_count(x, "mobile"))
-    df["TabletCount"] = df['DeviceCategories'].map(lambda x: device_count(x, "tablet"))
-
-
-def split_dataframe(df):
-    df["Has_Related"] = df["Sequence"].map(is_related)
-    ## Seperate out desktop and mobile journeys
-    desktop_journeys = df[df.DesktopCount > 0]
-    mobile_journeys = df[df.MobileCount > 0]
-
-    desk_rel_journeys = desktop_journeys[desktop_journeys.Has_Related]
-    mobile_rel_journeys = mobile_journeys[mobile_journeys.Has_Related]
-
-def chi2_test(obs):
-    chi2, p, dof, ex = stats.chi2_contingency(obs)
-    print(chi2, p, dof, ex)
-
-def compute_stats(df, df_related, desktop_journeys, mobile_journeys, desk_rel_journeys, mobile_rel_journeys, df_stats):
-    vol_all = df.Occurrences.sum()
-    vol_all_related = df[df.Has_Related].Occurrences.sum()
-    # Number of journeys coming from desktops
-    vol_desk = df["DesktopCount"].sum()
-    # Number of journeys coming from mobiles
-    vol_mobile = df["MobileCount"].sum()
-    # Number of journeys coming from tablets
-    vol_tablet = df["TabletCount"].sum()
-
-    # Compute number of journeys from specific device that include related links
-    # Don't base counting on occurrences, will include excluded device
-    vol_desk_rel = desk_rel_journeys.DesktopCount.sum()
-    vol_mobile_rel = mobile_rel_journeys.MobileCount.sum()
-
-    vols = [vol_all, vol_all_related,
-            vol_desk, vol_desk_rel,
-            vol_mobile, vol_mobile_rel]
-
-    percent_related = round((vol_all_related * 100) / vol_all, 2)
-    percent_from_desk = round((vol_desk * 100) / df.Occurrences.sum(), 2)
-    percent_from_mobile = round((vol_mobile * 100) / df.Occurrences.sum(), 2)
-    percent_from_tablet = round((vol_tablet * 100) / df.Occurrences.sum(), 2)
-    percent_from_desk_rel = round((vol_desk_rel * 100) / vol_desk, 2)
-    percent_from_mobile_rel = round((vol_mobile_rel * 100) / vol_mobile, 2)
-
-    percents = [100, percent_related,
-                percent_from_desk, percent_from_desk_rel,
-                percent_from_mobile, percent_from_mobile_rel]
-
-    shape_all = df.shape[0]
-    shape_all_rel = df[df.Has_Related].shape[0]
-    shape_desk = desktop_journeys.shape[0]
-    shape_desk_rel = desktop_journeys[desktop_journeys.Has_Related].shape[0]
-    shape_mobile = mobile_journeys.shape[0]
-    shape_mobile_rel = mobile_journeys[mobile_journeys.Has_Related].shape[0]
-
-    shapes = [shape_all, shape_all_rel,
-              shape_desk, shape_desk_rel,
-              shape_mobile, shape_mobile_rel]
-
-    df_stats["Volume"] = vols
-    df_stats["Percentage"] = percents
-    df_stats["Shape"] = shapes
+    column_eval([], df)
+    return None
 
 
 if __name__ == "__main__":
